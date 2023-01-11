747c868a2e04d5d3ba5bda1a1c0e16517dce758f
uniqueel <umarfaruqadam03@gmail.com>
2023-01-11 03:27:13 -0800

    cm

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
-# creating a file in /tmp directory
+# creates a file in /tmp
 
-file { 'school':
-  path       => '/tmp/school',
-  mode       => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  content    => 'I love Puppet',
+file { '/tmp/school':
+  content =>'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
